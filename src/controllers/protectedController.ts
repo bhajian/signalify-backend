d6a01763b3f3b5c44f03134945139951ee4df353
Behnam Hajian <bhajian@redhat.com>
2024-12-04 19:07:09 -0500

    add users api

--- src/controllers/protectedController.ts
+++ src/controllers/protectedController.ts
@@ -5,4 +5,13 @@ export const getProtected = (req: Request, res: Response): void => {
       message: "This is a protected route!",
       user: req.user, // The user data from the JWT token
     });
+  }; 
+
+  export const getUsers = (req: Request, res: Response): void => {
+    res.json({
+      data: {
+        users: ["user1", "user2", "user3"]
+      },
+      user: req.user, // The user data from the JWT token
+    });
   };
--- src/routes/protected.ts
+++ src/routes/protected.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getProtected } from "../controllers/protectedController";
+import { getProtected, getUsers } from "../controllers/protectedController";
 import { authenticate } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.get("/", authenticate, getProtected); // GET /protected with authentication
+router.get("/users", authenticate, getUsers);
 
 export default router;
